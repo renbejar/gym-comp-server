4f057c8840f3a43c787478943cf668b7dd190c3b
renbejar <jaredrenberg623gmail.com>
2024-03-12 23:39:02 -0600

    Set up basic Spring Boot application

---
+++ src/main/java/gym/comp/server/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package gym.comp.server.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
